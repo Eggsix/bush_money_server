394fa858231805aae6daecac2065fd24d3c5b0d3
Preston <plphan206@yahoo.com>
2016-08-11 12:15:02 -0700

    server with react front

--- router.js
+++ router.js
@@ -8,7 +8,7 @@ const requireSignin = passport.authenticate('local', { session: false });
 module.exports = function(app) {
 	app
 	.get('/', requireAuth, function(req, res) {
-		res.send({ hi: 'there' });
+		res.send({ message: 'Suber secret code is ABC123' });
 	})
 	.post('/signup', Auth.signup)
 	.post('/signin', requireSignin, Auth.signin);
