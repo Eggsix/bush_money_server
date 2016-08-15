6bb3f716b591e0ff9ea49bd7a98ef36e5b013297
Preston <plphan206@yahoo.com>
2016-08-15 09:59:13 -0700

    moved env

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,6 +1,8 @@
 const jwt = require('jwt-simple');
 const User =require('../models/user');
 
+const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 if (env === 'development') {
 	const config = require('../config');
 }
